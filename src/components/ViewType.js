d959e9ab6db6d03246cf4669ef7bc159b36848a2
Neil Brown <nebrown@hof.co.uk>
2018-09-13 17:25:15 +0100

    added state to view type buttons

--- src/components/ViewType.js
+++ src/components/ViewType.js
@@ -3,20 +3,45 @@ import Button from './Button/index';
 import ButtonStyles from './Button/styles.css';
 import Layout from '../Layout.css';
 
-const ViewType = (props) => {
-    return (
+class ViewType extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            selectedId: 0
+         }
+
+         this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler(type, index) {
+        this.setState(prevState => {
+            return { 
+                skipUpdate: prevState.selectedId === index,
+                selectedId: index 
+            };
+        }, () => {
+            if(!this.state.skipUpdate) {
+                this.props.handler(type)
+            }
+        });
+    }
+
+    render() { 
+        return (
             <section className={[Layout['flex-row'], Layout['vertical-centre']].join(' ')}>
                 <p>Data:</p>
-                <Button classes={ButtonStyles['secondary-button']}
-                                clickHandler={() => props.handler('full')}
+                <Button classes={[ButtonStyles['secondary-button'], this.state.selectedId === 0 ? ButtonStyles['secondary-button--selected'] : ''].join(' ')}
+                                clickHandler={() => this.clickHandler('full', 0)}
                                 iconStyle={ButtonStyles['secondary-button__icon']}
                                 text="Full" />
-                <Button classes={ButtonStyles['secondary-button']}
-                                clickHandler={() => props.handler('minimal')}
+                <Button classes={[ButtonStyles['secondary-button'], this.state.selectedId === 1 ? ButtonStyles['secondary-button--selected'] : ''].join(' ')}
+                                clickHandler={() => this.clickHandler('minimal', 1)}
                                 iconStyle={ButtonStyles['secondary-button__icon']}
                                 text="Minimal" />
             </section>
     );
+    }
 }
  
 export default ViewType;
