22f46cd96f1a76cfda7cb3cb8f1385c439a97a4f
Neil Brown <neilbrown08@hotmail.com>
2019-10-19 16:47:51 +0100

    added Sentry error logging

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import * as Sentry from '@sentry/browser';
+
 import Header from './components/Header';
 import Flights from './containers/Flights/index';
 import Styles from './App.module.css';
@@ -6,6 +8,8 @@ import ReactGA from 'react-ga';
 
 class App extends Component {
   componentDidMount() {
+    Sentry.init({dsn: "https://c59644d1c05645f0b81702d9dd38af59@sentry.io/1545393"});
+
     ReactGA.initialize('UA-143319467-1');
     ReactGA.pageview('/app');
   }
