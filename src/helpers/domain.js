6f5d3dd193294b9bbbbe12bcd253dbc427a627be
Neil Brown <neibrown08@hotmail.com>
2020-03-17 11:20:46 +0000

    added check for production to use correct domain

--- src/helpers/domain.js
+++ src/helpers/domain.js
@@ -1,2 +1,2 @@
-const domain = (process.env.NODE_ENV === 'production')? 'https://xc-league.herokuapp.com' : 'http://localhost:3000';
+const domain = (process.env.NODE_ENV === 'production') ? 'https://xc-league.herokuapp.com' : 'http://localhost:3000';
 export { domain };
