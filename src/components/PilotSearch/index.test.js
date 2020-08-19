a6d086449e9f57823b4d648766683c2accf50e26
Neil Brown <neilbrown08@hotmail.com>
2020-08-19 14:25:30 +0100

    adding more test

--- src/components/PilotSearch/index.test.js
+++ src/components/PilotSearch/index.test.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import * as effector from 'effector-react';
-import { render, fireEvent } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  screen,
+  getAllByText,
+  getByText
+} from '@testing-library/react';
+
+import '@testing-library/jest-dom';
 
 import { PilotSearch } from './index';
 
@@ -15,18 +23,53 @@ const pilots = [
   'David Thomson'
 ];
 
-const useStoreMock = jest.spyOn(effector, 'useStore');
-useStoreMock.mockReturnValue(pilots);
+const repeatPilots = [
+  'Neil Brown',
+  'Neil Charles',
+  'Neil Cruickshank',
+  'Neil Furmidge',
+  'Neil Hutchison',
+  'Neil McCain',
+  'Neil Morgan',
+  'Neil Morley',
+  'Neil Plant',
+  'Neil Roberts',
+  'Neil Rollings',
+  'Neil Speed',
+  'Neil Wheeler'
+];
 
-describe('my function or component', () => {
-  test('does the following', () => {
+describe('Pilot Search', () => {
+  test('Should reveal two pilot results', () => {
+    const useStoreMock = jest.spyOn(effector, 'useStore');
+    useStoreMock.mockReturnValue(pilots);
+
     const searchTerm = 'Thom';
-    const pattern = new RegExp(`${searchTerm}`, 'i');
-    const buttons = pilots.filter(pilot => pattern.test(pilot));
     const container = render(<PilotSearch data={pilots} />);
+
     const input = container.getByTestId('pilot-search');
     fireEvent.focus(input);
     fireEvent.change(input, { target: { value: searchTerm } });
-    expect(buttons.length).toBe(2);
+
+    const results = [
+      container.getByText('Adrian Thomas'),
+      container.getByText('David Thomson')
+    ];
+    expect(results.length).toBe(2);
+  });
+
+  test('Should reveal show more button', () => {
+    const useStoreMock = jest.spyOn(effector, 'useStore');
+    useStoreMock.mockReturnValue(repeatPilots);
+
+    const searchTerm = 'Neil';
+    const container = render(<PilotSearch data={repeatPilots} />);
+
+
+    const input = container.getByTestId('pilot-search');
+    fireEvent.focus(input);
+    fireEvent.change(input, { target: { value: searchTerm } });
+
+    expect(container.queryByText('Show more')).toBeInTheDocument();
   });
 });
