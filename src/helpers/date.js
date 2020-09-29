6d7c24e83e82dc5dfc6a7116749d19e22d3f24fb
Neil Brown <neilbrown08@hotmail.com>
2020-09-29 14:46:57 +0100

    Update date.js
    cleaner reduce method

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,16 +1,10 @@
 export const getDatesCount = data => {
-  let dataReduced = data.reduce((itemsArray, item) => {
-    itemsArray[item] = {
-      date: item,
-      count:
-        (itemsArray[item] && itemsArray[item].count
-          ? itemsArray[item].count
-          : 0) + 1
-    };
+  return data.reduce((itemsArray, item) => {
+    if(!itemsArray[item]) itemsArray[item] = { date: item, count: 0 };
+    const obj = itemsArray[item];
+    obj.count = obj.count + 1;
     return itemsArray;
-  }, []);
-
-  return Object.keys(dataReduced).map(item => dataReduced[item]);
+  }, []).filter(it => it);
 };
 
 export const getWeekDays = (baseDate, locale) => {
